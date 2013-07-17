c1243e120a77f479179d4babef0ce5ccacbb0313
MigrenusMaximus <felg4@hotmail.com>
2013-07-18 00:21:54 +0200

    Supressed a warning.

--- Dummies/src/DaProgram.java
+++ Dummies/src/DaProgram.java
@@ -1,10 +1,12 @@
 import java.io.IOException;
 
 public class DaProgram {
+	@SuppressWarnings("resource")
 	public static void main(String[] args) throws IOException {
 		
 		FileHandler mahFile = new FileHandler();
-		String fileName = new java.util.Scanner(java.lang.System.in).next();
+		String fileName;
+		fileName = new java.util.Scanner(java.lang.System.in).next();
 		java.lang.System.out.println(fileName);
 		
 		mahFile.setFileName(fileName);
