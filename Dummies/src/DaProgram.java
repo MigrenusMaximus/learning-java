67927e1a32fd4356dc73641b93c51f0f2e546b4d
MigrenusMaximus <felg4@hotmail.com>
2013-07-18 18:06:06 +0200

    Makes keys similar to Microsoft product keys.

--- Dummies/src/DaProgram.java
+++ Dummies/src/DaProgram.java
@@ -6,31 +6,31 @@
  * 
  */
 
-
+import static java.lang.System.out;
+import static java.lang.System.in;
+import java.util.Scanner;
 import java.io.IOException;
 
 public class DaProgram {
-	//@SuppressWarnings("resource")
 	public static void main(String[] args) throws IOException {
-		/*
-		EmployeeFromFile employeeFile = new EmployeeFromFile();
-		String fileName;
+		int keyBitValue = 0;
+		KeyGenerator[] key = new KeyGenerator[5];
+		for (int i = 0; i < 5; i++){
+			out.println("Key part " + (i+1) + " value:");
+			keyBitValue = new Scanner(in).nextInt(); //try to make it 290-400 for optimum results
+			key[i] = new KeyGenerator(keyBitValue); //anything lower than 240 or higher than 450 will cause it to hang
+			key[i].makeKey(); 						//cause it's impossible to make a combination with that value
+		}
 		
-		java.lang.System.out.println("Which file would you like to access?");
-		fileName = new java.util.Scanner(java.lang.System.in).next();
-		
-		employeeFile.setFileName(fileName);
-		employeeFile.fileInit();
-		employeeFile.getLines();
-		
-		employeeFile.empDataInit();
-		employeeFile.printEmployeeInfo();
-		*/
-		
-		KeyGenerator key = new KeyGenerator();
-		
-		key.makeKey();
-		key.printKey();
-		
+		for (int i = 0; i < 5; i++){
+			if(i == 4){
+				key[i].printKey();
+				break;
+			}
+			
+			key[i].printKey();
+			out.print("-");
+			
+		}
 	}
 }
--- Dummies/src/KeyGenerator.java
+++ Dummies/src/KeyGenerator.java
@@ -13,19 +13,22 @@ import static java.lang.System.out;
 
 public class KeyGenerator extends FileHandler {
 	private char[] key = new char[5];
+	private int keyValue;
+	
+	public KeyGenerator(int keyValue){
+		this.keyValue = keyValue;
+	}
 	
 	public void makeKey() {
 		int placeHolder = 0;
-		while (key[0]+key[1]+key[2]+key[3]+key[4]!=368){
+		while (key[0]+key[1]+key[2]+key[3]+key[4]!=this.keyValue){
 			for (int i = 0; i < 5; i++){
 				placeHolder = 0;
 				while ( !((placeHolder >= 48 && placeHolder <= 57) || 
 						(placeHolder >= 65 && placeHolder <= 90)) ) {
 					placeHolder = new Random().nextInt(122) + 1;
-					//out.println(placeHolder);
 				}
 				this.key[i] = (char) placeHolder;
-				out.println(this.key[i]);
 			}
 		}
 	}
@@ -34,6 +37,5 @@ public class KeyGenerator extends FileHandler {
 		for (int i = 0; i < 5; i++){
 			out.print(this.key[i]);
 		}
-		out.println();
 	}
 }
